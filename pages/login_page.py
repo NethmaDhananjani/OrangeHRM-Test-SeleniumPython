ab54bd302c8ac6148d87f09f4da18af393f6df7c
NethmaDhananjani <nethma@limarktech.com>
2024-09-20 11:08:26 +0530

    forget password and home page actions

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,6 +8,12 @@ class LoginPage:
         self.password_field = (By.NAME, "password")
         self.login_button = (By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button')
 
+        self.forget_password_link = (By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[4]/p')
+        self.linkedin_icon = (By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[3]/div[1]/a[1]')
+        self.facebook_icon = (By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[3]/div[1]/a[2]')
+        self.twitter_icon = (By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[3]/div[1]/a[3]')
+        self.youtube_icon = (By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[3]/div[1]/a[4]')
+
     def load(self):
         self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
@@ -22,3 +28,37 @@ class LoginPage:
     def click_login_button(self):
         login_button_element = self.driver.find_element(*self.login_button)
         login_button_element.click()
+
+    def click_forgot_password(self):
+        """Click the 'Forgot Password' link."""
+        forgot_password_element = self.driver.find_element(*self.forget_password_link)
+        forgot_password_element.click()
+
+    def click_linkedin_icon(self):
+        """Click the LinkedIn icon and switch to the new tab."""
+        linkedin_element = self.driver.find_element(*self.linkedin_icon)
+        linkedin_element.click()
+        # Switch to the new tab
+        windows = self.driver.window_handles
+        self.driver.switch_to.window(windows[-1])  # Switch to the newly opened LinkedIn tab
+
+    def click_facebook_icon(self):
+        """Click the Facebook icon and switch to the new tab."""
+        facebook_element = self.driver.find_element(*self.facebook_icon)
+        facebook_element.click()
+        windows = self.driver.window_handles
+        self.driver.switch_to.window(windows[-1])
+
+    def click_twitter_icon(self):
+        """Click the Twitter icon and switch to the new tab."""
+        twitter_element = self.driver.find_element(*self.twitter_icon)
+        twitter_element.click()
+        windows = self.driver.window_handles
+        self.driver.switch_to.window(windows[-1])
+
+    def click_youtube_icon(self):
+        """Click the YouTube icon and switch to the new tab."""
+        youtube_element = self.driver.find_element(*self.youtube_icon)
+        youtube_element.click()
+        windows = self.driver.window_handles
+        self.driver.switch_to.window(windows[-1])
--- tests/login_test.py
+++ tests/login_test.py
@@ -1,5 +1,10 @@
+from telnetlib import EC
+
 import pytest
-import time  # Import the time module to add sleep delays
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 from pages.login_page import LoginPage
 from util.base import BaseTest
 
@@ -8,14 +13,19 @@ class TestLogin(BaseTest):
         self.keep_browser_open = True  # Set flag to keep the browser open
         self.loginpage = LoginPage(self.driver)
 
-        # Load the login page
+        # Step 1: Load the login page
         self.loginpage.load()
 
-        # Enter username
+        # Step 2: Enter login credentials
         self.loginpage.enter_username("Admin")
-
-        # Enter password
         self.loginpage.enter_password("admin123")
 
+        # Step 3: Click the login button
         self.loginpage.click_login_button()
-        time.sleep(2)  # Pause for 2 seconds 
+        time.sleep(2)
+
+        # Step 4: Wait for the login to complete (or verify login)
+        # WebDriverWait(self.driver, 10).until(
+        #     EC.presence_of_element_located((By.ID, "dashboard"))
+        # )
+
